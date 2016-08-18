bff23abd000317849b5a1807f6c00079736cf7ef
run6337125 <run6337125@gmail.com>
2016-08-18 20:21:02 +0800

    Revert "Revert "commit message""
    This reverts commit c541d20c3bc9dd2a0d98d6e313eff3f0f40f5405.

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<form name="FORM" method="POST" action="connect.php">
+帳號：<input type="text" name="id" /> <br>
+密碼：<input type="password" name="pw" /> <br>
+<input type="submit" name="button" value="登入" />&nbsp;&nbsp;
+<a href="register.php">申請帳號</a>
+</form>
+</html>
